4902e4adce48ac1a669101c78299906e3c0143f9
Nastya Rusina <nastya@union.ai>
2022-04-24 15:40:02 -0700

    chore: move src to a new location
    Signed-off-by: Nastya Rusina <nastya@union.ai>

--- packages/zapp/console/src/components/Literals/__stories__/Scalar.stories.tsx
+++ packages/zapp/console/src/components/Literals/__stories__/Scalar.stories.tsx
@@ -1,8 +1,8 @@
 import { storiesOf } from '@storybook/react';
 import { Scalar } from 'models/Common/types';
 import * as React from 'react';
-import { Card, CardContent } from '@material-ui/core';
-import { LiteralMapViewer } from '../LiteralMapViewer';
+import { ScalarValue } from '../Scalar/ScalarValue';
+import { CardDecorator } from './CardDecorator';
 import {
   binaryScalars,
   blobScalars,
@@ -11,45 +11,22 @@ import {
   primitiveScalars,
   schemaScalars,
 } from './scalarValues';
-import { DeprecatedLiteralMapViewer } from '../DeprecatedLiteralMapViewer';
 
 const stories = storiesOf('Literals/Scalar', module);
+stories.addDecorator(CardDecorator);
 
-var renderScalars = (scalars: Dictionary<Scalar>) => {
-  const literals = {};
-
+const renderScalars = (scalars: Dictionary<Scalar>) =>
   Object.entries(scalars).map(([label, value]) => {
-    literals[label] = { scalar: value, value: 'scalar' };
+    return <ScalarValue key={label} label={label} scalar={value as Scalar} />;
   });
-
-  const map = { literals };
-  return (
-    <>
-      <div style={{ display: 'flex' }}>
-        <div style={{ marginRight: '16px' }}>
-          OLD
-          <Card>
-            <CardContent>
-              <DeprecatedLiteralMapViewer map={map} />
-            </CardContent>
-          </Card>
-        </div>
-        <div>
-          NEW
-          <Card>
-            <CardContent>
-              <LiteralMapViewer map={map} />
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-    </>
-  );
-};
 
 stories.add('Binary', () => <>{renderScalars(binaryScalars)}</>);
 stories.add('Blob', () => <>{renderScalars(blobScalars)}</>);
 stories.add('Error', () => <>{renderScalars(errorScalars)}</>);
-stories.add('NoneType', () => <>{renderScalars([noneTypeScalar])}</>);
+stories.add('NoneType', () => (
+  <>
+    <ScalarValue label="empty_value" scalar={noneTypeScalar} />
+  </>
+));
 stories.add('Primitive', () => <>{renderScalars(primitiveScalars)}</>);
 stories.add('Schema', () => <>{renderScalars(schemaScalars)}</>);
--- packages/zapp/console/src/components/Navigation/DefaultAppBarContent.tsx
+++ packages/zapp/console/src/components/Navigation/DefaultAppBarContent.tsx
@@ -1,19 +1,11 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import classnames from 'classnames';
-import { AppInfo, VersionInfo } from '@flyteconsole/components';
-import { FlyteLogo } from '@flyteconsole/ui-atoms';
+import { FlyteLogo } from 'components/common/FlyteLogo';
 import { useCommonStyles } from 'components/common/styles';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Routes } from 'routes/routes';
-import { FeatureFlag, useFeatureFlag } from 'basics/FeatureFlags';
-import { useAdminVersion } from 'components/hooks/useVersion';
-import { env } from 'common/env';
 import { UserInformation } from './UserInformation';
-import { OnlyMine } from './OnlyMine';
-import t, { patternKey } from './strings';
-
-const { version: platformVersion } = require('../../../package.json');
 
 const useStyles = makeStyles((theme: Theme) => ({
   spacer: {
@@ -28,37 +20,13 @@ const useStyles = makeStyles((theme: Theme) => ({
 export const DefaultAppBarContent: React.FC = () => {
   const commonStyles = useCommonStyles();
   const styles = useStyles();
-
-  const isFlagEnabled = useFeatureFlag(FeatureFlag.OnlyMine);
-  const { adminVersion } = useAdminVersion();
-
-  const versions: VersionInfo[] = [
-    {
-      name: t('versionConsoleUi'),
-      version: platformVersion,
-      url: `https://github.com/flyteorg/flyteconsole/releases/tag/v${platformVersion}`,
-    },
-    {
-      name: t('versionAdmin'),
-      version: adminVersion,
-      url: `https://github.com/flyteorg/flyteadmin/releases/tag/v${adminVersion}`,
-    },
-    {
-      name: t('versionGoogleAnalytics'),
-      version: t(patternKey('gaDisable', env.DISABLE_GA)),
-      url: 'https://github.com/flyteorg/flyteconsole#google-analytics',
-    },
-  ];
-
   return (
     <>
       <Link className={classnames(commonStyles.linkUnstyled)} to={Routes.SelectProject.path}>
-        <FlyteLogo size={32} />
+        <FlyteLogo size={32} variant="dark" />
       </Link>
       <div className={styles.spacer} />
-      {isFlagEnabled && <OnlyMine />}
       <UserInformation />
-      <AppInfo versions={versions} documentationUrl="https://docs.flyte.org/en/latest/" />
     </>
   );
 };
